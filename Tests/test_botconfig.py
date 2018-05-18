7d474035f98facf05ae9e29597dbe24025bb17cf
Mark Nowiasz <buckaroo+github@midworld.de>
2018-05-18 16:47:12 +0200

    Tests, Bookmarks. Commit before refactoring

--- Tests/test_botconfig.py
+++ Tests/test_botconfig.py
@@ -8,7 +8,7 @@ def test_botname():
 
 
 class TestAcess(object):
-    _test_data = [
+    _test_data = (
         # ID, Access granted, exception raised (add)
         (1, True, False),
         (2, False, False),
@@ -16,7 +16,7 @@ class TestAcess(object):
         (4, True, False),
         (4, True, True),
         (1, True, True)
-    ]
+    )
 
     @classmethod
     def setup_class(cls):
@@ -47,3 +47,57 @@ class TestAcess(object):
         else:
             self._test_config.remove_access(telegram_id)
             assert not self._test_config.has_access(telegram_id)
+
+
+class TestBookmarks(object):
+    _test_data = (
+        # Bookmark name, title_id, playlist_id
+        ("current", "12345", None),
+        ("A", "6cdef0a", "abc12345"),
+        ("MyBookmark", "adef134", None),
+        ("Foo", "qras124dzu", "rerqzwe2")
+    )
+
+    @classmethod
+    def setup_class(cls):
+        cls._test_config = botconfig.Config()
+        for entry in cls._test_data:
+            name, title_id, playlist_id = entry
+            cls._test_config.set_bookmark(name, title_id, playlist_id)
+
+    @pytest.mark.parametrize("bookmark_name, title_id, playlist_id", _test_data)
+    def test_get_bookmark(self, bookmark_name, title_id, playlist_id):
+        title, playlist = self._test_config.get_bookmark(bookmark_name)
+        assert title == title_id
+        assert playlist == playlist_id
+
+    # Test if a bookmark really gets overwritten
+    def test_overwrite_bookmark(self):
+        before = ("1234", "5678")
+        after = ("abcd", None)
+        bookmarkname = "FooTestOverride"
+        self._test_config.set_bookmark(bookmarkname, before[0], before[1])
+        title, playlist = self._test_config.get_bookmark(bookmarkname)
+
+        assert title == before[0]
+        assert playlist == before[1]
+
+        self._test_config.set_bookmark(bookmarkname, after[0], after[1])
+        title, playlist = self._test_config.get_bookmark(bookmarkname)
+
+        assert title == after[0]
+        assert playlist == after[1]
+
+    def test_nonexistent_bookmark(self):
+        with  pytest.raises(KeyError):
+            title, playlist = self._test_config.get_bookmark("NONEXISTENT")
+
+    def test_clear_nonexistent_bookmkar(self):
+        with pytest.raises(KeyError):
+            self._test_config.clear_bookmark("NONEXISTENT")
+
+    @pytest.mark.parametrize("bookmark_name, title_id, playlist_id", _test_data)
+    def test_clear_bookmark(self, bookmark_name, title_id, playlist_id):
+        self._test_config.clear_bookmark(bookmark_name)
+        with  pytest.raises(KeyError):
+            title, playlist = self._test_config.get_bookmark(bookmark_name)
--- botconfig.py
+++ botconfig.py
@@ -5,8 +5,9 @@ class Config(object):
     """ The global config object """
     botname = "MyBot"
     __access_set = set()
+    __bookmark_set = {}
 
-    def has_access(self, telegram_id: int)-> bool:
+    def has_access(self, telegram_id: int) -> bool:
         """
         :param telegram_id: The telegram ID
         :type telegram_id: int
@@ -41,3 +42,39 @@ class Config(object):
 
         self.__access_set.remove(telegram_id)
 
+    def set_bookmark(self, bookmark_name: str, title_id: str, playlist_id: str = None):
+        """
+
+        :param bookmark_name: The name of the bookmark ("current", "foo", "bar"..)
+        :type bookmark_name: str
+        :param title_id: Spotify's title ID - base62 coded
+        :type title_id: str
+        :param playlist_id: The (optional) playlist ID - base62 coded
+        :type playlist_id: str
+
+        Sets a bookmark. If already prsenet, the bookmark will be overwritten
+        """
+        self.__bookmark_set[bookmark_name] = (title_id, playlist_id)
+
+    def get_bookmark(self, bookmark_name: str) -> (str, str):
+        """
+
+        :param bookmark_name: The name of the bookmark ("current", "a")
+        :type bookmark_name: str
+        :return: The ID of the title and (optionally) playlist
+        :rtype: (str, str)
+
+        Gets a bookmark. If the name of the bookmark doesn't exist a KeyError will be raised
+        """
+        return self.__bookmark_set[bookmark_name]
+
+    def clear_bookmark(self, bookmark_name: str):
+        """
+
+        :param bookmark_name: The name of the bookmark ("current", "a")
+        :type bookmark_name: str
+
+        Clears the bookmark. If the bookmark doesn't exist a KeyError will be raised
+        """
+
+        del self.__bookmark_set[bookmark_name]
