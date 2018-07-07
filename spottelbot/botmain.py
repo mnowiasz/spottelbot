a2a436a36d96f5926f780bf95df1b23458989cf0
Mark Nowiasz <buckaroo+github@midworld.de>
2018-07-07 17:59:04 +0200

    botmain now reflects the cahgnes in load_config

--- spottelbot/botmain.py
+++ spottelbot/botmain.py
@@ -1,11 +1,9 @@
 """ The Main file"""
-import configparser
 import logging
 import sys
 from pathlib import Path
 
 from spottelbot import botconfig
-from spottelbot import botexceptions
 from spottelbot import spotifycontroller
 from spottelbot import telegramcontroller
 
@@ -44,36 +42,18 @@ def botmain():
     if not configfile:
         print("Unable to find configfile!")
         exit(1)
-    try:
-        config = botconfig.BotConfig()
-        config.load_config(configfile)
+
+    config = botconfig.BotConfig()
+    error = config.load_config(configfile)
+
+    if not error:
         spotify_controller = spotifycontroller.SpotifyController(config)
         spotify_controller.connect()
         telegram_controller = telegramcontroller.TelegramController(config, spotify_controller)
         telegram_controller.connect()
-    except botexceptions.InvalidUser as invalid:
-        print("Invalid User " + invalid.bad_id)
+    else:
+        print(error)
         exit(1)
-    except botexceptions.InvalidBookmark as invalid:
-        print("Invalid bookmark " + invalid.invalid_bookmark)
-        exit(1)
-    except botexceptions.DuplicateUsers as duplicate:
-        print("Duplicate User " + duplicate.duplicate_id)
-        exit(1)
-    except botexceptions.MissingUsers:
-        print("No user defined!")
-        exit(1)
-    except botexceptions.MissingSection as missing:
-        print("Missing config section " + missing.missing_section)
-        exit(1)
-    except botexceptions.MissingTelegramToken:
-        print("Missing telegram token")
-        exit(1)
-    except botexceptions.MissingSpotifyUsername:
-        print("Missing spotify username")
-    except configparser.MissingSectionHeaderError:
-        print("No sections in configfile")
-
 
 if __name__ == '__main__':
     botmain()
