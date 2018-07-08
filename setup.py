06f73527b6a863aabc19fb45c4e1ea369a1f574e
Mark Nowiasz <buckaroo+github@midworld.de>
2018-07-08 14:26:19 +0200

    Forgot to change setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='spottelbot',
-    version='0.1.1',
+    version='0.2.0',
     description='A telegram bot implementing missing spotify features',
     author="@mnowiasz",
     author_email="mark+spottelbot@nowiasz.de",
